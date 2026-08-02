8e758478fb833865bb6e86ba80c22a0182a085fd
dagaben <gabriel.stevko@gmail.com>
2026-08-02 17:06:31 +0200

    Remove incomplete titlePoster.js

--- src/titlePoster.js
+++
@@ -1,2 +0,0 @@
-// Auto-generated title poster (Vampire Ducks graphic)
-export const TITLE_POSTER_SRC = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAoHBwgHBgoICAgLCgoLDhgQDg0NDh0VFhEYIx8lJCIfIiEmKzcvJik0K
